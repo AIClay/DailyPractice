e9e734412b531b200c2e50897ff9da7725224ec8
Aaron I. Clay <AIClay@users.noreply.github.com>
2018-04-16 06:09:41 -0400

    extneral array of webSafe colors
    this external file is used by 04162018.htm ['randomAnimatedBoxes'] to provide an extensible array of webSafe colors for random color effect. Extensible because I may decide to add a larger array of colors to be randomized.   :)  Just a nifty widget that depends heavily on "window.requestAnimationFrame()" call.

---
+++ webSafe.js
@@ -0,0 +1,12 @@
+//javascript source file for websafe colors
+function externalHitTest()
+{
+	console.log("external file loaded fine.");
+}
+// source of webSafe color: https://websafecolors.info/color-chart
+var arCols = [
+"#000000","#000033","#000066","#000099","#0000cc","#0000ff",
+"#003300","#003333","#003366","#003399","#0033cc","#0033ff",
+"#006600","#006633","#006666","#006699","#0066cc","#0066ff",
+"#009900","#009933","#009966","#009999","#0099cc","#0099ff"
+];
