f64e74eec7a41f2c696641c19ccf96a7b97eccba
Aaron I. Clay <AIClay@users.noreply.github.com>
2018-04-23 06:51:50 -0400

    changed colors
    changed colors for more ambient ones. will break other practices if arrays not updated.

--- webSafe.js
+++ webSafe.js
@@ -1,12 +1,16 @@
+
 //javascript source file for websafe colors
 function externalHitTest()
 {
 	console.log("external file loaded fine.");
 }
-// source of webSafe color: https://websafecolors.info/color-chart
+// 
+// source(s) of webSafe color: https://websafecolors.info/color-chart
+//                             html-color-codes.com 
+// 216 Possible
+// 
 var arCols = [
-"#000000","#000033","#000066","#000099","#0000cc","#0000ff",
-"#003300","#003333","#003366","#003399","#0033cc","#0033ff",
-"#006600","#006633","#006666","#006699","#0066cc","#0066ff",
-"#009900","#009933","#009966","#009999","#0099cc","#0099ff"
+"#FFFFFF","#CCCCCC","#999999","#666666","#333333", // grey's
+"#CCCC00","#FFFF00","#CC9933","#FFFF33","#FFFF66","#FFFF99", // yellows
+
 ];
